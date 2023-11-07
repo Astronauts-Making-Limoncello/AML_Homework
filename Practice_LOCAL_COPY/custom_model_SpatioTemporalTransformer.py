2bdf078b9c19292a3c2d1f353c42ea708ab3485e
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2023-11-07 14:49:23 +0100

    log val loss during each stage, rather than epoch in custom_model_SpatioTemporalTransformer

--- Practice_LOCAL_COPY/custom_model_SpatioTemporalTransformer.py
+++ Practice_LOCAL_COPY/custom_model_SpatioTemporalTransformer.py
@@ -360,20 +360,20 @@ def train(data_loader,vald_loader, path_to_save_model=None):
               progress_bar.update(task_id=epoch_task, advance=1/(n_train_batches + n_val_batches))
 
 
+              if running_loss/n < val_loss_best:
+                val_loss_best = running_loss/n
+                val_loss_best_epoch = epoch
+
+                torch.save({
+                  'epoch': epoch + 1,
+                  'model_state_dict': model.state_dict(),
+                  'optimizer_state_dict': optimizer.state_dict(),
+                  'train_loss': train_loss,
+                  'val_loss': val_loss
+                  }, f"{ckpt_dir}/{model_name}_best_val_loss.pt")
+
+
           val_loss.append(running_loss.detach().cpu()/n)
-          if running_loss/n < val_loss_best:
-            val_loss_best = running_loss/n
-            val_loss_best_epoch = epoch
-
-            torch.save({
-              'epoch': epoch + 1,
-              'model_state_dict': model.state_dict(),
-              'optimizer_state_dict': optimizer.state_dict(),
-              'train_loss': train_loss,
-              'val_loss': val_loss
-              }, f"{ckpt_dir}/{model_name}_best_val_loss.pt")
-
-
 
 
 
@@ -409,7 +409,8 @@ def train(data_loader,vald_loader, path_to_save_model=None):
     "best_loss/train": np.array(train_loss).min(),
     "best_loss_epoch/train": np.array(train_loss).argmin() + epoch_offset,
     "best_loss/val": np.array(val_loss).min(),
-    "best_loss_epoch/val": np.array(val_loss).argmin() + epoch_offset
+    "best_loss_in_step/train": train_loss_best,
+    "best_loss_in_step/val": val_loss_best
   })
 
 
